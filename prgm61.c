f41cfb097072df35cc8257b4b32fa625c46c9f54
vigneshsoundararajan <31505640+vigneshsoundararajan@users.noreply.github.com>
2018-03-12 15:52:12 +0530

    Create prgm61.c

---
+++ prgm61.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+	int n,i;
+	char a[10];
+	printf("\n neter the string");
+	scanf("%s",&a);
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("\n %c",a[i]);
+	}
+
+}
